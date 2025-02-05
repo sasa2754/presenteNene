b2a4ec5c90d2e146b8494c2ddb9c70a8c880fe05
sasa2754 <sasamor5@gmail.com>
2025-02-04 21:43:48 -0300

    aaaa

---
+++ src/app/timeline/page.tsx
@@ -0,0 +1,10 @@
+// src/app/timeline/page.tsx
+export default function Timeline() {
+    return (
+      <div>
+        <h1>Timeline</h1>
+        {/* Não feito ainda */}
+      </div>
+    );
+  }
+  
